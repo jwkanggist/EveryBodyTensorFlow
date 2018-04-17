40680cfd90fffa4bd6d0b1560b3600744d1d6d50
jwkangmacpro <jwkang10@gmail.com>
2018-04-17 17:51:17 +0900

    DevMento 강의중 수정사항 반영

--- python_basic/helloworld_example.py
+++ python_basic/helloworld_example.py
@@ -2,3 +2,19 @@
 
 print ('helloworld')
 print 'helloworld'
+
+
+for i in range(0,4):
+    print (' %s- th number' % i)
+
+
+name = 'kang'
+
+if name is not 'kang':
+    print 'right!'
+else:
+    print 'wrong!'
+
+
+
+
--- tf_basic/ex_runTFmulmat.py
+++ tf_basic/ex_runTFmulmat.py
@@ -18,10 +18,10 @@ import tensorflow as tf
 # 이 연산자는 기본 그래프의 노드로 추가됩니다.
 #
 # 생성자에 의해 반환된 값(matrix1)은 Constant 연산의 출력을 나타냅니다.
-matrix1 = tf.constant([[3., 3.]])
+matrix1 = tf.constant([[3., 3.]]) # 1 by 2
 
 # 2x1 행렬을 만드는 또 다른 Constant를 생성합니다.
-matrix2 = tf.constant([[2.],[2.]])
+matrix2 = tf.constant([[2.],[2.]]) # 2 by 1
 
 # 'matrix1'과 'matrix2'를 입력으로 받는 Matmul 연산을 생성합니다.
 # 반환값인 'product'는 행렬을 곱한 결과를 나타냅니다.
